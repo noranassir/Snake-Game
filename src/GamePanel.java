8b2846dc12380a3c67c8f7fd2864c6458ca53d59
Nora Nassir <noraknassir@gmail.com>
2024-03-23 19:59:21 +0100

    Created 3 main classes for game. Created constructor for class GameFrame. Created method headers for class GamePanel.

---
+++ src/GamePanel.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    public void draw(Graphics graphics){
+
+    }
+    public void components(Graphics graphics){
+
+    }
+    public void move(){
+
+    }
+    public void checkCollision(){
+
+    }
+    public void checkFood(){
+
+    }
+    public void startGame(){
+
+    }
+    public void gameOver(){
+
+    }
+    public class AKeyAdapter extends KeyAdapter{
+        public void pressedKey(KeyEvent event){
+
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
