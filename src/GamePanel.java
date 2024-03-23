67ceef94be41f7abaa569c1b6e5b6faa342b8b9d
Nora Nassir <noraknassir@gmail.com>
2024-03-23 20:31:21 +0100

    Created constructor for class GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,11 +6,29 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class GamePanel extends JPanel implements ActionListener {
+    static final int PANEL_WIDTH = 500;
+    static final int PANEL_HEIGHT = 500;
+    static final int COMPONENT_SIZE = 25;
+    static final int GAME_UNIT =(PANEL_WIDTH*PANEL_HEIGHT)/COMPONENT_SIZE;
+    int x[] = new int[GAME_UNIT];
+    int y[] = new int[GAME_UNIT];
+    Timer timer;
+    boolean running = false;
+
+    GamePanel(){
+        this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
+        this.setBackground(Color.black);
+        this.addKeyListener(new AKeyAdapter());
+        startGame();
+    }
 
     public void draw(Graphics graphics){
 
     }
     public void components(Graphics graphics){
+
+    }
+    public void generateFood(){
 
     }
     public void move(){
@@ -23,7 +41,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
     public void startGame(){
-
+        generateFood();
     }
     public void gameOver(){
 
