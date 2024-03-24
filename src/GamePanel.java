b2cd4087b4a7b6ee6323b064e246af1928d4aa8e
Nora Nassir <noraknassir@gmail.com>
2024-03-24 16:52:19 +0100

    Wrote method for checking collision with snake body. Added inner class to override keypressed method of KeyAdapter Class to handle keyboard input. Added focusable to GamePanel constructor to allow panel to receive input from keyboard

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -21,18 +21,22 @@ public class GamePanel extends JPanel implements ActionListener {
     private int bodyComponents = 10;
     private char direction = 'R';
 
+    private int eatenFood = 0;
+
     Random random = new Random();
 
     GamePanel(){
         this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
         this.setBackground(Color.black);
         this.addKeyListener(new AKeyAdapter());
+        this.setFocusable(true); // Allow the panel to receive keyboard input
+        this.requestFocusInWindow(); // Request focus for the panel
         startGame();
     }
 
     public void draw(Graphics g){
 
-        //draw food
+        //Draw food
         int red = random.nextInt(256);
         int green = random.nextInt(256);
         int blue = random.nextInt(256);
@@ -40,7 +44,7 @@ public class GamePanel extends JPanel implements ActionListener {
         g.setColor(randomColor);
         g.fillOval(foodX,foodY, COMPONENT_SIZE, COMPONENT_SIZE);
 
-        //draw body
+        //Draw snake
         for(int i = 0; i < bodyComponents; i++){
             if(i == 0){
                 g.setColor(Color.green);
@@ -85,10 +89,18 @@ public class GamePanel extends JPanel implements ActionListener {
         }
     }
     public void checkCollision(){
-
+        for(int i = bodyComponents - 1; i > 0; i--){
+            if(x[0] == x[i] && y[0] == y[i]){
+                running = false;
+            }
+        }
     }
     public void checkFood(){
-
+        if(x[0] == foodX && y[0] == foodY){
+            bodyComponents++;
+            eatenFood++;
+            generateFood();
+        }
     }
     public void startGame(){
         generateFood();
@@ -100,8 +112,30 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
     public class AKeyAdapter extends KeyAdapter{
-        public void pressedKey(KeyEvent event){
-
+        @Override
+        public void keyPressed(KeyEvent event){
+            switch (event.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R'){
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L'){
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D'){
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U'){
+                        direction = 'D';
+                    }
+                    break;
+            }
         }
     }
 
